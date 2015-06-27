9d3cd2bb90de256564f250c3714a5c17c3559391
Williams Olivera <liamsolivera@gmail.com>
2015-06-27 14:33:38 -0300

    bitwise expression class and grammar

---
+++ src/com/language/model/expression/BitwiseExpression.java
@@ -0,0 +1,31 @@
+package com.language.model.expression;
+
+public class BitwiseExpression extends Expression {
+	/* PRECEDENCE:
+	 	- EXP
+	 	- TIMES, DIV, DIV_INT, MOD
+	 	- PLUS, MINUS
+	*/
+	public static final String OR_BIT = "OrBit";
+	public static final String AND_BIT = "AndBit";
+	public static final String XOR_BIT = "XorBit";
+	public static final String LSHIFT = "LShift";
+	public static final String RSHIFT = "RShift";
+	public static final String NOT_BIT = "NotBit";
+	
+	public BitwiseExpression() {
+		super();
+	}
+	
+	public BitwiseExpression(String type, Object value, Expression left, Expression right) {
+		super(type,value,left,right);
+	}
+	
+	public BitwiseExpression(String type, Expression left, Expression right) {
+		super(type, left, right);
+	}
+	
+	public static Expression create(String type, Object value, Expression left, Expression right) {
+		return new BitwiseExpression(type, value, left, right);
+	}	
+}
