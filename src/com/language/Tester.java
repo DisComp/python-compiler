8df89a1f9250e16056f6da9f0095bb44e654c19d
Marcio Rodriguez <marciorodriguez91@gmail.com>
2015-07-03 17:51:53 -0300

    ajusto un poquito el while

--- src/com/language/Tester.java
+++ src/com/language/Tester.java
@@ -11,7 +11,7 @@ public class Tester {
 
 	public static void main(String[] args) throws IOException {
 				
-		BufferedReader br = new BufferedReader(new FileReader("tests/Test2_IF_STATEMENT.txt"));
+		BufferedReader br = new BufferedReader(new FileReader("tests/Test3_WHILE_STATEMENT.txt"));
 		StringBuilder sb = new StringBuilder();
         String line = br.readLine();
         
