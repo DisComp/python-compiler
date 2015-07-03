fc0ce95336207d1d655e7580809a9f654c1debf2
Williams Olivera <liamsolivera@gmail.com>
2015-07-03 01:07:23 -0300

    predefined functions advance

--- src/com/language/Tester.java
+++ src/com/language/Tester.java
@@ -11,7 +11,7 @@ public class Tester {
 
 	public static void main(String[] args) throws IOException {
 				
-		BufferedReader br = new BufferedReader(new FileReader("tests/TestAssignment.txt"));
+		BufferedReader br = new BufferedReader(new FileReader("tests/TestPredefinedDictionaryFunctions.txt"));
 		StringBuilder sb = new StringBuilder();
         String line = br.readLine();
         
---
+++ src/com/language/model/expression/PredefinedFunctionExpression.java
@@ -0,0 +1,71 @@
+package com.language.model.expression;
+
+
+public class PredefinedFunctionExpression extends Expression{
+	
+	/* Dictionaries */
+	public static final String HAS_KEY_FUNC = "HasKeyFunc";
+	public static final String ITEMS_FUNC = "ItemsFunc";
+	public static final String KEYS_FUNC = "KeysFunc";
+	public static final String POP_FUNC = "PopFunc";
+	public static final String VALUES_FUNC = "ValuesFunc";
+	
+	/* Strings */
+	public static final String COUNT_FUNC = "CountFunc";
+	public static final String FIND_FUNC = "FindFunc";
+	public static final String JOIN_FUNC = "JoinFunc";
+	public static final String SPLIT_FUNC = "SplitFunc";
+	public static final String REPLACE_FUNC = "ReplaceFunc";
+	public static final String LENGTH_FUNC = "LengthFunc";
+	
+	/* Lists */
+	public static final String APPEND_FUNC = "AppendFunc";
+	public static final String EXTEND_FUNC = "ExtendFunc";
+	public static final String INDEX_FUNC = "IndexFunc";
+	public static final String INSERT_FUNC = "InsertFunc";
+	public static final String SIZE_FUNC = "SizeFunc";
+	
+	/* Input/Output */
+	public static final String RAW_INPUT_FUNC = "RawInputFunc";
+	public static final String PRINT_FUNC = "PrintFunc";
+	
+	/* Type conversion */
+	public static final String  INT_FUNC = "IntFunc"; 
+	public static final String  FLOAT_FUNC = "FloatFunc";
+	public static final String  STR_FUNC = "StrFunc";
+	public static final String  TUPLE_FUNC = "TupleFunc";
+	public static final String  LIST_FUNC = "ListFunc";
+	public static final String DICT_FUNC= "DictFunc";
+
+	/* Long */
+	public static final String  LONG_FUNC = "LongFunc";
+
+	/* String */
+	public static final String  STRING_FUNC = "StringFunc";
+		
+	/* Others */
+	public static final String  TYPE_FUNC = "TypeFunc"; 
+	
+	
+	public PredefinedFunctionExpression() {
+		super();
+	}
+	
+	public PredefinedFunctionExpression(String type, Object value, Expression left, Expression right) {
+		super(type,value,left,right);
+	}
+	
+	public PredefinedFunctionExpression(String type, Expression left, Expression right) {
+		super(type, left, right);
+	}
+	
+	public static Expression createDictionaryFunction(Expression dict_left, Expression expr) {
+		/*e.g: D.has_key(x) saves D on left and x on right */
+		return new PredefinedFunctionExpression(expr.getType(), dict_left, expr);
+	}
+	
+	public static Expression createDictionaryFunctionElement(String type, Expression right) {
+		/* Save dictionary function parameters on the right (dictionary object will be on the left) */
+		return new PredefinedFunctionExpression(type, null, right);
+	} 
+}
