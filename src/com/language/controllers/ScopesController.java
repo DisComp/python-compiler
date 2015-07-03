18e37b18eae1365e5a9237bdad8f7dd006ce2682
Marcio Rodriguez <marciorodriguez91@gmail.com>
2015-07-02 23:15:32 -0300

    arreglo control de tabs

--- src/com/language/controllers/ScopesController.java
+++ src/com/language/controllers/ScopesController.java
@@ -2,12 +2,13 @@ package com.language.controllers;
 
 public class ScopesController {
 	
-	private static ScopesController instance = null;
-	private int tabsContados = 0;
-	private int tabsEsperados = 0;
+	private static ScopesController instance = null;	
+	private int tabsContados;
+	private int tabsEsperados;
 	
 	private ScopesController() {
-    	
+		tabsContados = 0;
+		tabsEsperados = 0;
     }
 	
 	public static ScopesController getInstance() {
@@ -19,25 +20,31 @@ public class ScopesController {
 	
 	public void openScope(String scope/*se puede cambir el tipo*/){
 		tabsEsperados++;
+		System.out.println("Scope: "+scope+" - Expected tabs: " +  tabsEsperados);
 	}
 	
-	public void closeScope(String scope/*se puede cambir el tipo*/){
-		tabsEsperados--;
-	}
+	//public void closeScope(String scope){
+	//	tabsEsperados--;
+	//}
 	
 	public void finDeInstruccion(){
-		if(tabsContados==tabsEsperados-1){//se cierra el scope
+		/*if(tabsContados==tabsEsperados-1){//se cierra el scope
 			tabsEsperados--;
 		}
-		else if (tabsEsperados>tabsContados){
-			System.out.println("Hay menos tabs de los esperados");
+		else*/
+		//System.out.println("lei instr");
+		
+		if (tabsEsperados>tabsContados){
+			System.out.println("Hay "+tabsContados+" tab y se esperaban: "+tabsEsperados);
 		}
+		tabsContados = 0;
 	}
 	
 	public void consumirTab(){
 		tabsContados++;
+		//System.out.println("cosnumo tab");
 		if (tabsEsperados<tabsContados){
-			System.out.println("Hay mas tabs de los esperados");
+			System.out.println("Hay "+tabsContados+" tab y se esperaban: "+tabsEsperados);
 		}
 	}
 }
