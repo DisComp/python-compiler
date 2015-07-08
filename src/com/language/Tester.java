bc2d586ac6d10b9f36cf751e11d196f5bca8e788
dgarcia <dani24gl@gmail.com>
2015-07-07 21:04:35 -0300

    - Creating tests for the ArithmeticalExpression class - Make advances in the BooleanExpression class

--- src/com/language/Tester.java
+++ src/com/language/Tester.java
@@ -4,16 +4,21 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+import com.language.model.expression.ArithmeticalExpression;
 import com.language.model.expression.Expression;
 import com.language.parser.ExpressionParser;
+import com.language.tests.Tests;
 
 public class Tester {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, Exception {
 
 		BufferedReader br = new BufferedReader(new FileReader("tests/testVacio.txt"));
 		StringBuilder sb = new StringBuilder();
         String line = br.readLine();
+        
+        // Creating the Arithmetical test //
+        Tests.doArithmeticalTest();
         
         while (line != null) {         
 			 sb.append(line);
